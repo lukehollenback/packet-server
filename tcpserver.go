c1294501a3dbecf7c32bb70767ddce53134aa812
Luke Hollenback <8144622+treebasher@users.noreply.github.com>
2020-04-04 19:05:42 -0600

    Began documenting and rounding-out interface.

--- tcpserver.go
+++ tcpserver.go
@@ -17,6 +17,8 @@ type Client struct {
 type server struct {
 	address                  string // Address to open connection: localhost:9999
 	config                   *tls.Config
+	listener                 net.Listener
+	clients                  []*Client
 	onNewClientCallback      func(c *Client)
 	onClientConnectionClosed func(c *Client, err error)
 	onNewMessage             func(c *Client, message string)
@@ -37,22 +39,69 @@ func (c *Client) listen() {
 	}
 }
 
-// Send text message to client
-func (c *Client) Send(message string) error {
-	_, err := c.conn.Write([]byte(message))
-	return err
+func (c *Client) close() {
+	c.conn.Close()
 }
 
-// Send bytes to client
+//
+// Send sends the specified message to the client.
+//
+func (c *Client) Send(message string) error {
+	return c.SendBytes([]byte(message))
+}
+
+//
+// SendAll sends the specified message to all clients currently connected to the server. If any
+// individual send operation fails, future sends will be terminated and the relevant error will be
+// returned.
+//
+func (s *server) SendAll(message string) error {
+	for _, e := range s.clients {
+		err := e.Send(message)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+//
+// SendBytes sends the specified bytes to the client.
+//
 func (c *Client) SendBytes(b []byte) error {
 	_, err := c.conn.Write(b)
 	return err
 }
 
+//
+// SendBytesAll sends the specified bytes to all clients currently connected to the server. If any
+// individual send operation fails, future sends will be terminated and the relevant error will be
+// returned.
+//
+func (s *server) SendBytesAll(b []byte) error {
+	for _, e := range s.clients {
+		err := e.SendBytes(b)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+//
+// Conn returns the current connection to the client.
+//
 func (c *Client) Conn() net.Conn {
 	return c.conn
 }
 
+//
+// Close closes the current connection to the client.
+//
 func (c *Client) Close() error {
 	return c.conn.Close()
 }
@@ -72,28 +121,58 @@ func (s *server) OnNewMessage(callback func(c *Client, message string)) {
 	s.onNewMessage = callback
 }
 
-// Listen starts network server
-func (s *server) Listen() {
-	var listener net.Listener
+//
+// Start starts the server if it has not already been started, or errors otherwise.
+//
+func (s *server) Start() {
+	//
+	// (Re)-initialize necessary members of the server structure.
+	//
+	s.clients = make([]*Client, 0)
+
+	//
+	// Attempt to fire up a the server.
+	//
 	var err error
+
 	if s.config == nil {
-		listener, err = net.Listen("tcp", s.address)
+		s.listener, err = net.Listen("tcp", s.address)
 	} else {
-		listener, err = tls.Listen("tcp", s.address, s.config)
+		s.listener, err = tls.Listen("tcp", s.address, s.config)
 	}
+
 	if err != nil {
 		log.Fatal("Error starting TCP server.")
 	}
-	defer listener.Close()
 
+	//
+	// Make sure that the server will always get cleaned up, no matter what happens to end execution.
+	//
+	defer s.listener.Close()
+
+	//
+	// Loop infinitely to accept new connections and spin off a handler thread for each.
+	//
 	for {
-		conn, _ := listener.Accept()
+		conn, _ := s.listener.Accept()
+
 		client := &Client{
 			conn:   conn,
 			Server: s,
 		}
+
+		s.clients = append(s.clients, client)
+
 		go client.listen()
 	}
+}
+
+func (s *server) Stop() {
+	for _, e := range s.clients {
+		e.Close()
+	}
+
+	s.listener.Close()
 }
 
 // Creates new tcp server instance
--- tcpserver_test.go
+++ tcpserver_test.go
@@ -40,7 +40,83 @@ func TestBasicLifecycle(t *testing.T) {
     connectinClosed = true
   })
 
-  go server.Listen()
+  go server.Start()
+
+  //
+  // Give the server some time to bind.
+  //
+  // NOTE: Although server.Listen() is a blocking call, we are running it asynchronously in a
+  //  goroutine for the sake of this test.
+  //
+  time.Sleep(10 * time.Millisecond)
+
+  //
+  // Connect to the server as a new client and sent it a test message.
+  //
+  conn, err := net.Dial("tcp", "localhost:9999")
+
+  if err != nil {
+    t.Fatal("Failed to connect to test server")
+  }
+
+  conn.Write([]byte(TestMessage))
+
+  conn.Close()
+
+  //
+  // Give the server a chance to recieve the new connection and the new message.
+  //
+  time.Sleep(10 * time.Millisecond)
+
+  //
+  // Assert that the server's handlers fired and that the expected messages came back.
+  //
+  if newClient != true {
+    t.Error("The \"OnNewClient\" event handler never fired.")
+  }
+
+  if messageReceived != true {
+    t.Error("The \"OnNewMessage\" event handler never fired.")
+  }
+
+  if messageText != TestMessage {
+    t.Error("A message was recieved, but it was not equal to what was expected.")
+  }
+
+  if connectinClosed != true {
+    t.Error("The \"OnClientConnectionClosed\" event handler never fired.")
+  }
+}
+
+func TestBasicLifecycleAgain(t *testing.T) {
+  //
+  // Define variables upon which we will state and assert proper functionality.
+  //
+  var messageReceived bool
+  var messageText string
+  var newClient bool
+  var connectinClosed bool
+
+  //
+  // Create a new server and register event handlers that will set variables against which we can
+  // run some assertions.
+  //
+  server := buildTestServer()
+
+  server.OnNewClient(func(c *Client) {
+    newClient = true
+  })
+
+  server.OnNewMessage(func(c *Client, message string) {
+    messageReceived = true
+    messageText = message
+  })
+
+  server.OnClientConnectionClosed(func(c *Client, err error) {
+    connectinClosed = true
+  })
+
+  go server.Start()
 
   //
   // Give the server some time to bind.
