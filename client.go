0321e9f473fd3461532ef11e04c1726ad92a2593
Luke Hollenback <8144622+treebasher@users.noreply.github.com>
2020-04-11 16:03:18 -0600

    Moved client-related structure and methods to unique file.

---
+++ client.go
@@ -0,0 +1,87 @@
+package tcpserver
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+)
+
+//
+// Client holds info about a single client connection.
+//
+type Client struct {
+	conn   net.Conn // Literal connection to the client.
+	server *Server  // The server that the client belongs to.
+}
+
+//
+// Close closes the current connection to the client.
+//
+func (c *Client) Close() {
+	c.conn.Close()
+}
+
+//
+// Send sends the specified message to the client.
+//
+func (c *Client) Send(message string) error {
+	return c.SendBytes([]byte(message))
+}
+
+//
+// SendBytes sends the specified bytes to the client.
+//
+func (c *Client) SendBytes(b []byte) error {
+	_, err := c.conn.Write(b)
+	return err
+}
+
+//
+// listen reads and processes new messages from the client while it is connected. It is intended to
+// be run in its own goroutine per connected client.
+//
+func (c *Client) listen() {
+	//
+	// Make sure that, even if something goes wrong, we close the client connection.
+	//
+	defer c.Close()
+
+	//
+	// Execute the registered "new client" event handler.
+	//
+	c.server.onNewClientCallback(c)
+
+	//
+	// Create a buffer reader to read recieved messages from the client and begin doing so in a loop.
+	//
+	reader := bufio.NewReader(c.conn)
+	for {
+		//
+		// Attempt to block and read the next message from the client. If this fails for any reason
+		// (e.g. an actual error or a disconnect), handle it accordingly.
+		//
+		message, err := reader.ReadString('\n')
+
+		if err != nil {
+			if err == io.EOF {
+				log.Printf("Client at %s has disconnected.", c.conn.RemoteAddr())
+			} else {
+				log.Printf("Buffer read for client at %s failed (%s). Connection will be closed.",
+					c.conn.RemoteAddr(), err)
+			}
+
+			c.Close()
+			c.server.onClientConnectionClosed(c, err)
+			c.server.forgetClient(c)
+
+			return
+		}
+
+		//
+		// If we get this far, we recieved a valid message from the client. Thus, execute the registered
+		// message handler.
+		//
+		c.server.onNewMessage(c, message)
+	}
+}
--- server.go
+++ server.go
@@ -1,21 +1,11 @@
 package tcpserver
 
 import (
-	"bufio"
 	"crypto/tls"
-	"io"
 	"log"
 	"net"
 	"time"
 )
-
-//
-// Client holds info about a single client connection.
-//
-type Client struct {
-	conn   net.Conn // Literal connection to the client.
-	server *Server  // The server that the client belongs to.
-}
 
 //
 // Server holds info about an actual server instance.
@@ -32,83 +22,12 @@ type Server struct {
 }
 
 //
-// Close closes the current connection to the client.
-//
-func (c *Client) Close() {
-	c.conn.Close()
-}
-
-//
-// Send sends the specified message to the client.
-//
-func (c *Client) Send(message string) error {
-	return c.SendBytes([]byte(message))
-}
-
-//
-// listen reads and processes new messages from the client while it is connected. It is intended to
-// be run in its own goroutine per connected client.
-//
-func (c *Client) listen() {
-	//
-	// Make sure that, even if something goes wrong, we close the client connection.
-	//
-	defer c.Close()
-
-	//
-	// Execute the registered "new client" event handler.
-	//
-	c.server.onNewClientCallback(c)
-
-	//
-	// Create a buffer reader to read recieved messages from the client and begin doing so in a loop.
-	//
-	reader := bufio.NewReader(c.conn)
-	for {
-		//
-		// Attempt to block and read the next message from the client. If this fails for any reason
-		// (e.g. an actual error or a disconnect), handle it accordingly.
-		//
-		message, err := reader.ReadString('\n')
-
-		if err != nil {
-			if err == io.EOF {
-				log.Printf("Client at %s has disconnected.", c.conn.RemoteAddr())
-			} else {
-				log.Printf("Buffer read for client at %s failed (%s). Connection will be closed.",
-					c.conn.RemoteAddr(), err)
-			}
-
-			c.Close()
-			c.server.onClientConnectionClosed(c, err)
-			c.server.forgetClient(c)
-
-			return
-		}
-
-		//
-		// If we get this far, we recieved a valid message from the client. Thus, execute the registered
-		// message handler.
-		//
-		c.server.onNewMessage(c, message)
-	}
-}
-
-//
 // SendAll sends the specified message to all clients currently connected to the server. If any
 // individual send operation fails, future sends will be terminated and the relevant error will be
 // returned.
 //
 func (s *Server) SendAll(message string) error {
 	return s.SendBytesAll([]byte(message))
-}
-
-//
-// SendBytes sends the specified bytes to the client.
-//
-func (c *Client) SendBytes(b []byte) error {
-	_, err := c.conn.Write(b)
-	return err
 }
 
 //
